9b2d98dc17547caeee22000fa07edbf0565d8ed2
Sophie Somerville <sophie.somerville@nhs.net>
2025-12-01 22:17:44 +0000

    part 2 solution

--- src/day01/main.rs
+++ src/day01/main.rs
@@ -4,14 +4,16 @@ fn main() {
     println!("Part 2: {}", part2(input));
 }
 
+fn parse_input(input: &str) -> impl Iterator<Item = i32> {
+    input.split_terminator("\n").map(|line| {
+        let turns = line[1..].parse::<i32>().unwrap();
+        if line.starts_with('L') { -turns } else { turns }
+    })
+}
+
 fn part1(input: &str) -> i32 {
     let start_position: i32 = 50;
-    input
-        .split_terminator("\n")
-        .map(|line| {
-            let turns = line[1..].parse::<i32>().unwrap();
-            if line.starts_with('L') { -turns } else { turns }
-        })
+    parse_input(input)
         .fold(
             (start_position, 0),
             |(last_position, zero_count), movement| {
@@ -22,6 +24,32 @@ fn part1(input: &str) -> i32 {
         .1
 }
 
-fn part2(_input: &str) -> i32 {
-    0
+fn part2(input: &str) -> i32 {
+    let start_position: i32 = 50;
+    parse_input(input)
+        .fold(
+            (start_position, 0),
+            |(last_position, zero_count), movement| {
+                let mut new_zero_count = zero_count;
+                let mut new_position = last_position;
+                for n in 0..movement.abs() {
+                    if movement < 0 {
+                        new_position -= 1
+                    } else {
+                        new_position += 1
+                    }
+                    if new_position == -1 {
+                        new_position = 99
+                    }
+                    if new_position == 100 {
+                        new_position = 0
+                    }
+                    if new_position == 0 {
+                        new_zero_count += 1
+                    }
+                }
+                (new_position, new_zero_count)
+            },
+        )
+        .1
 }
