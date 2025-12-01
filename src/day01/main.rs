fbdaa612bb95be7ece0d7f0a596523c455232ad3
Sophie Somerville <sophie.somerville@nhs.net>
2025-12-01 21:04:09 +0000

    part 1 solution

--- src/day01/main.rs
+++ src/day01/main.rs
@@ -4,8 +4,22 @@ fn main() {
     println!("Part 2: {}", part2(input));
 }
 
-fn part1(_input: &str) -> i32 {
-    0
+fn part1(input: &str) -> i32 {
+    let start_position: i32 = 50;
+    input
+        .split_terminator("\n")
+        .map(|line| {
+            let turns = line[1..].parse::<i32>().unwrap();
+            if line.starts_with('L') { -turns } else { turns }
+        })
+        .fold(
+            (start_position, 0),
+            |(last_position, zero_count), movement| {
+                let new_position = (last_position + movement).rem_euclid(100);
+                (new_position, zero_count + (new_position == 0) as i32)
+            },
+        )
+        .1
 }
 
 fn part2(_input: &str) -> i32 {
