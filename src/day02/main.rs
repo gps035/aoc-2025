6de7b3214c42bc34c3cc93c3635b1e1821916d7a
Sophie Somerville <sophie.somerville@nhs.net>
2025-12-02 08:32:49 +0000

    part 1

--- src/day02/main.rs
+++ src/day02/main.rs
@@ -4,8 +4,18 @@ fn main() {
     println!("Part 2: {}", part2(input));
 }
 
-fn part1(_input: &str) -> i32 {
-    0
+fn part1(input: &str) -> i64 {
+    input
+        .split(",")
+        .map(|range| range.split_once("-").unwrap())
+        .map(|(start, end)| (start.parse::<i64>().unwrap(), end.parse::<i64>().unwrap()))
+        .flat_map(|(start, end)| start..end)
+        .filter(|id| {
+            let s = id.to_string();
+            let mid = s.len() / 2;
+            s[..mid] == s[mid..]
+        })
+        .sum()
 }
 
 fn part2(_input: &str) -> i32 {
