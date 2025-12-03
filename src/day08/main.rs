672666da83e07ef66641ec71c559e3cdb0b700b7
Sophie Somerville <sophie.somerville@nhs.net>
2025-12-03 16:04:05 +0000

    Add timings

--- src/day08/main.rs
+++ src/day08/main.rs
@@ -1,7 +1,18 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 fn main() {
     let input = include_str!("input.txt");
+    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
     println!("Part 1: {}", part1(input));
+    let end = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    println!("Time elapsed: {:?}", end - start);
+
+    println!("\n");
+
+    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
     println!("Part 2: {}", part2(input));
+    let end = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    println!("Time elapsed: {:?}", end - start);
 }
 
 fn part1(_input: &str) -> i32 {
