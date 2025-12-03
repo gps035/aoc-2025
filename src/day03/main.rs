2af97994d0be467b57550b2cfa59e62823b087d8
Sophie Somerville <sophie.somerville@nhs.net>
2025-12-03 13:53:58 +0000

    Formatting

--- src/day03/main.rs
+++ src/day03/main.rs
@@ -5,31 +5,27 @@ fn main() {
 }
 
 fn pick_batteries(input: &str, battery_count: usize) -> impl Iterator<Item = i64> {
-    input
-        .split_terminator("\n")
-        .map(move |bank| {
-            let mut next_possible_index = 0;
-            let mut chosen = vec!['0'; battery_count];
-            for x in 0..chosen.len() {
-                let start = next_possible_index;
-                let end = bank.len() - (chosen.len() - x);
-                for (i,c) in bank[start..=end].chars().enumerate() {
-                    if c > chosen[x] {
-                        next_possible_index = start +i+1;
-                        chosen[x] = c;
-                    }
+    input.split_terminator("\n").map(move |bank| {
+        let mut next_possible_index = 0;
+        let mut chosen = vec!['0'; battery_count];
+        for x in 0..chosen.len() {
+            let start = next_possible_index;
+            let end = bank.len() - (chosen.len() - x);
+            for (i, c) in bank[start..=end].chars().enumerate() {
+                if c > chosen[x] {
+                    next_possible_index = start + i + 1;
+                    chosen[x] = c;
                 }
             }
-            String::from_iter(chosen)
-                .parse::<i64>()
-                .unwrap()
-        })
+        }
+        String::from_iter(chosen).parse::<i64>().unwrap()
+    })
 }
 
 fn part1(input: &str) -> i64 {
-    return pick_batteries(input, 2).sum()
+    pick_batteries(input, 2).sum()
 }
 
 fn part2(input: &str) -> i64 {
-    return pick_batteries(input, 12).sum()
+    pick_batteries(input, 12).sum()
 }
