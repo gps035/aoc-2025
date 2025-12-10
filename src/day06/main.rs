baac5184f654cb72a590fc2d76b60b0d24ea66bb
Sophie Somerville <sophie.somerville@nhs.net>
2025-12-10 20:39:06 +0000

    part 1

--- src/day06/main.rs
+++ src/day06/main.rs
@@ -5,8 +5,26 @@ fn main() {
     utilities::run_solution(include_str!("input.txt"), part1, part2);
 }
 
-fn part1(_input: &str) -> i64 {
-    0
+fn part1(input: &str) -> i64 {
+    let mut lines = input
+        .split_terminator("\n")
+        .map(|line| line.split_whitespace().collect::<Vec<&str>>())
+        .collect::<Vec<Vec<&str>>>();
+    let operators = lines.pop().unwrap();
+    operators
+        .iter()
+        .enumerate()
+        .map(|(index, op)| {
+            let numbers = lines
+                .iter()
+                .map(|line| line[index])
+                .map(|item| item.parse::<i64>().unwrap());
+            if *op == "*" {
+                return numbers.product::<i64>();
+            }
+                return numbers.sum();
+        })
+        .sum()
 }
 
 fn part2(_input: &str) -> i64 {
