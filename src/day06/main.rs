b17d7c113bced9f2d466c6a619b6a32821d13884
Sophie Somerville <sophie.somerville@nhs.net>
2025-12-10 21:04:11 +0000

    horrible part 2 solution

--- src/day06/main.rs
+++ src/day06/main.rs
@@ -27,6 +27,35 @@ fn part1(input: &str) -> i64 {
         .sum()
 }
 
-fn part2(_input: &str) -> i64 {
-    0
+fn part2(input: &str) -> i64 {
+    let lines = input
+        .split_terminator("\n")
+        .map(|line| line.as_bytes())
+        .collect::<Vec<&[u8]>>();
+    let mut sums = Vec::new();
+    let mut current_numbers = Vec::new();
+    for index in (0..lines[0].len()).rev() {
+         let mut column = String::from_utf8(lines.iter().map(|line| {
+            (*line)[index]
+        }).collect::<Vec<u8>>()).unwrap();
+        if column.ends_with("*") {
+            column.pop();
+            current_numbers.push(column.trim().parse().unwrap());
+            sums.push(current_numbers.iter().product::<i64>());
+            current_numbers.clear();
+            continue
+        }
+        if column.ends_with("+") {
+            column.pop();
+            current_numbers.push(column.trim().parse().unwrap());
+            sums.push(current_numbers.iter().sum::<i64>());
+            current_numbers.clear();
+            continue
+        }
+        if column.trim().is_empty() {
+            continue
+        }
+        current_numbers.push(column.trim().parse().unwrap());
+    }
+    sums.iter().sum()
 }
