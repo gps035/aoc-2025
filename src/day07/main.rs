d6cc47b72d50cdfff744e55bc6e9b6b1529f285f
Sophie Somerville <sophie.somerville@nhs.net>
2025-12-11 00:51:39 +0000

    cleaned up

--- src/day07/main.rs
+++ src/day07/main.rs
@@ -1,14 +1,11 @@
-use std::collections::HashSet;
-
 #[path = "../utilities.rs"]
 mod utilities;
 
+const START_INDICATOR: u8 = b'S';
+const SPLITTER_INDICATOR: u8 = b'^';
+
 fn main() {
     utilities::run_solution(include_str!("input.txt"), part1, part2);
-}
-
-fn get_start_column(line: &[u8]) -> usize {
-    line.iter().position(|&r| r == b'S').unwrap()
 }
 
 fn get_lines(input: &str) -> Vec<&[u8]> {
@@ -17,45 +14,42 @@ fn get_lines(input: &str) -> Vec<&[u8]> {
 
 fn part1(input: &str) -> i64 {
     let lines = get_lines(input);
-    let mut columns = HashSet::from([get_start_column(&lines[0])]);
+    let mut active: Vec<bool> = lines[0].iter().map(|&c| c == START_INDICATOR).collect();
     let mut splits = 0;
+
     for line in lines {
-        let mut new_columns = HashSet::new();
-        for beam in &columns {
-            if line[*beam] == b'^' {
-                new_columns.insert(beam - 1);
-                new_columns.insert(beam + 1);
+        let mut next_active = active.clone();
+        for (i, &is_active) in active.iter().enumerate() {
+            if is_active && line[i] == SPLITTER_INDICATOR {
+                next_active[i - 1] = true;
+                next_active[i + 1] = true;
+                next_active[i] = false;
                 splits += 1;
-                continue;
             }
-            new_columns.insert(*beam);
         }
-        columns = new_columns;
+        active = next_active;
     }
     splits
 }
 
 fn part2(input: &str) -> i64 {
     let lines: Vec<&[u8]> = get_lines(input);
-    let rows = lines.len();
-    let columns = lines[0].len();
+    let mut path_counts = vec![vec![0; lines[0].len()]; lines.len()];
 
-    let mut paths_to_bottom = vec![vec![0; columns]; rows];
-
-    for row in (0..rows).rev() {
-        for col in 0..columns {
-            let mut value = 1;
-            if row < rows - 1 {
-                let below = &paths_to_bottom[row + 1];
-                value = below[col];
-                if lines[row][col] == b'^' {
-                    value = below[col - 1] + below[col + 1];
-                }
-            }
-
-            paths_to_bottom[row][col] = value;
+    for row in (0..path_counts.len()).rev() {
+        for col in 0..path_counts[row].len() {
+            path_counts[row][col] = if row == lines.len() - 1 {
+                1
+            } else if lines[row][col] == SPLITTER_INDICATOR {
+                path_counts[row + 1][col - 1] + path_counts[row + 1][col + 1]
+            } else {
+                path_counts[row + 1][col]
+            };
         }
     }
 
-    paths_to_bottom[0][get_start_column(lines[0])]
+    path_counts[0][lines[0]
+        .iter()
+        .position(|&pos| pos == START_INDICATOR)
+        .expect("There is no starting indicator")]
 }
